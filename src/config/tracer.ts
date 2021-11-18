eea02d68a5946a26b1a0dee80c2e3a8feafcb987
gustavo-evaristo <gug.henri1@gmail.com>
2021-11-18 14:10:57 -0300

    datadog

---
+++ src/config/tracer.ts
@@ -0,0 +1,8 @@
+import tracer from 'dd-trace';
+
+tracer.init({
+    logInjection: true,
+    
+});
+
+export default tracer;
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import './config/tracer';
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    const { name, password, confirm_password } = req.body;
+
+    const { token } = req.headers;
+
+    if (!token) return res.status(401).json({ message: 'Unauthorized user' });
+
+    if (token !== 'ABC') res.status(401).json({ message: 'Invalid Token' });
+
+    if (!name || !password || !confirm_password) return res.status(400).json({ message: 'Invalid fields' });
+
+
+    if (password !== confirm_password) return res.status(400).json({ message: `Password doesn't match` });
+
+    const user = { name, password };
+
+    return res.status(200).json(user);
+})
+
+app.listen(3000, () => console.log('server is running'));
